47f9a90362844760f1bd0e5c255e759fd2c10a52
George Cham <357611628@qq.com>
2022-03-22 09:42:00 +0800

    Create 去除CSDN代码示例登录后复制的限制.js

---
+++ 去除CSDN代码示例登录后复制的限制.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         去除CSDN代码示例登录后复制的限制
+// @namespace    https://github.com/zhzhch335/myTampermonkey
+// @version      1.0
+// @description  解决未登录时CSDN不能复制代码的问题
+// @author       zhzhch335
+// @match        http*://blog.csdn.net/*/article/details/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=csdn.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    document.querySelectorAll("code").forEach(function(item) {
+        item.style = item.style + ";user-select: auto !important;";
+        return item;
+    })
+})();
