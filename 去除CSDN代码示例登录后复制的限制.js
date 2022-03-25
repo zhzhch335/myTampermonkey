573cdc0f453a0dcb1660f67df252797f0fcb9df7
George Cham <357611628@qq.com>
2022-03-25 08:40:11 +0800

    不同版本页面重写sign方法的trycatch

--- 去除CSDN代码示例登录后复制的限制.js
+++ 去除CSDN代码示例登录后复制的限制.js
@@ -9,52 +9,55 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     // Your code here...
     // 将所有代码区域变为可选
-    document.querySelectorAll("code").forEach(function(item) {
+    document.querySelectorAll("code").forEach(function (item) {
         item.style = item.style + ";user-select: text !important;";
         return item;
     })
     // 将所有登录复制按钮变成全选
-    document.querySelectorAll(".hljs-button").forEach(function(item) {
+    document.querySelectorAll(".hljs-button").forEach(function (item) {
         item.dataset.title = "复制全部";
         return item;
     })
-    // 重写登录复制方法
-    window.hljs.signin = e => {
-        var preNode = e.path.filter(item => item.tagName == "PRE")[0];
-        // 选中一段文字
-        let selection = window.getSelection();
-        let range = document.createRange();
-        range.selectNode(preNode);
-        selection.removeAllRanges();
-        selection.addRange(range);
-        // 执行复制命令
-        document.execCommand('copy', false, null);
-        e.target.dataset.title = "复制成功";
-        setTimeout(() => {
-            e.target.dataset.title = "复制全部";
-        },1000);
+    try {
+        // 重写登录复制方法
+        window.hljs.signin = e => {
+            var preNode = e.path.filter(item => item.tagName == "PRE")[0];
+            // 选中一段文字
+            let selection = window.getSelection();
+            let range = document.createRange();
+            range.selectNode(preNode);
+            selection.removeAllRanges();
+            selection.addRange(range);
+            // 执行复制命令
+            document.execCommand('copy', false, null);
+            e.target.dataset.title = "复制成功";
+            setTimeout(() => {
+                e.target.dataset.title = "复制全部";
+            }, 1000);
+        }
+        // 重写另一个登录方法（需要去除行号和版权声明）
+        window.mdcp.signin = e => {
+            var preNode = e.path.filter(item => item.tagName == "CODE")[0];
+            // 选中一段文字
+            let selection = window.getSelection();
+            let range = document.createRange();
+            range.selectNode(preNode);
+            selection.removeAllRanges();
+            selection.addRange(range);
+            // 执行复制命令
+            document.execCommand('copy', false, null);
+            e.target.dataset.title = "复制成功";
+            setTimeout(() => {
+                e.target.dataset.title = "复制全部";
+            }, 1000);
+        }
     }
-    // 重写另一个登录方法（需要去除行号和版权声明）
-    window.mdcp.signin = e => {
-        var preNode = e.path.filter(item => item.tagName == "CODE")[0];
-        // 选中一段文字
-        let selection = window.getSelection();
-        let range = document.createRange();
-        range.selectNode(preNode);
-        selection.removeAllRanges();
-        selection.addRange(range);
-        // 执行复制命令
-        document.execCommand('copy', false, null);
-        e.target.dataset.title = "复制成功";
-        setTimeout(() => {
-            e.target.dataset.title = "复制全部";
-        },1000);
-    }
+    catch { }
     // 解除关注才能查看全文的限制
     $('#article_content').removeAttr("style");
     $('.hide-article-box').remove();
